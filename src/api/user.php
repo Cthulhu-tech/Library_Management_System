4cf142da09632edf74bf2a9c1725d41b19f05c95
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-02 23:31:16 +0300

    user query params check add

--- src/api/user.php
+++ src/api/user.php
@@ -9,18 +9,75 @@ class User extends Database
 
     function __construct()
     {
+        header('Content-Type: application/json');
+
         parent::__construct();
+    }
+
+    private function checkParams()
+    {
+
+        if (isset($_GET['limit'], $_GET['offset'])) {
+
+            $this->limit = $_GET['limit'];
+            $this->offset = $_GET['offset'];
+
+            $this->createDatabase();
+
+            $this->result = $this->getDB()->query("SELECT COUNT(*) as count FROM user_library");
+
+            $limitUser = $this->result->fetchAll(PDO::FETCH_ASSOC)[0]['count'];
+
+            if ($limitUser === 0) {
+
+                echo $this->messageResponse(400, 'sorry your database is empty');
+
+                return false;
+            }
+
+            if ($this->limit === null || $this->offset === null) {
+
+                echo $this->messageResponse(400, 'exceeding the limit. max limit = ' . $limitUser);
+
+                return false;
+            }
+
+            if ($this->limit > 50) {
+
+                $this->limit = 50;
+            }
+
+            if ($this->limit < 0) {
+
+                $this->limit = 10;
+            }
+
+            return true;
+        }
+
+        echo $this->messageResponse(400, 'all fields are required');
+
+        return false;
     }
 
     public function getAllUsers()
     {
+
+        $check = $this->checkParams();
+
+        if (!$check) {
+
+            return 0;
+        }
+
         $this->createDatabase();
 
         $this->result = $this->getDB()->query("SELECT * FROM user_library");
 
-        while ($row = $this->result->fetch(PDO::FETCH_ASSOC)) {
+        $row = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
 
-            echo $row;
-        }
+        print_r($row);
+
+        $this->closeConnection();
     }
 }
