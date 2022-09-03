665a5ae78c4b49761c2def0f82b8f97abb83a4fb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-03 18:25:39 +0300

    user class update

--- src/api/user.php
+++ src/api/user.php
@@ -1,87 +1,39 @@
 <?php
 
-require __DIR__ . '/../utils/database/db.php';
+require __DIR__ . '/../utils/check/checkUser.php';
+require_once __DIR__ . '/../utils/database/db.php';
 
 class User extends Database
 {
 
     private $result;
+    private $checkUser;
 
     function __construct()
     {
 
+        $this->checkUser = new UserCheck();
         parent::__construct();
-    }
-
-    private function checkParams()
-    {
-
-        if (isset($_GET['limit'], $_GET['offset'])) {
-
-            $this->limit = $_GET['limit'];
-            $this->offset = $_GET['offset'];
-
-            if(!is_int($_GET['limit']) || !is_int($_GET['offset'])) {
-
-                echo $this->messageResponse(400, 'sorry, limit or offset is not a number.');
-                
-                return false;
-
-            }
-
-            $this->createDatabase();
-
-            $this->result = $this->getDB()->query("SELECT COUNT(*) as count FROM user_library");
-
-            $limitUser = $this->result->fetchAll(PDO::FETCH_ASSOC)[0]['count'];
-
-            if ($limitUser === 0) {
-
-                echo $this->messageResponse(400, 'sorry your database is empty');
-
-                return false;
-            }
-
-            if ($this->offset > $limitUser) {
-
-                echo $this->messageResponse(400, 'exceeding the offset. max offset = ' . $limitUser);
-
-                return false;
-            }
-
-            if ($this->limit > 50) {
-
-                $this->limit = 50;
-            }
-
-            if ($this->limit < 0) {
-
-                $this->limit = 10;
-            }
-
-            return true;
-        }
-
-        echo $this->messageResponse(400, 'all fields are required');
-
-        return false;
     }
 
     public function getUsers()
     {
 
-        $check = $this->checkParams();
+        $protected = $this->checkUser->checkParams();
 
-        if (!$check) {
+        if (!$protected) {
 
-            return 0;
+            return false;
         }
+
+        $limit = $this->checkUser->getThisLimit();
+        $offset = $this->ckeckcheckUser->getThisOffset();
 
         $this->createDatabase();
 
         $this->result = $this->getDB()->prepare("SELECT * FROM user_library WHERE LIMIT ? , ?");
 
-        $this->result->execute(array( $this->offset,  $this->limit));
+        $this->result->execute(array($offset,  $limit));
 
         $row = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
 
@@ -90,9 +42,136 @@ class User extends Database
         $this->closeConnection();
     }
 
-    public function getUser() {
+    public function getUser()
+    {
 
-        
+        $protected = $this->checkUser->userCheckAllParameters(false);
 
+        if (!$protected) {
+
+            return false;
+        }
+
+        $name = $this->checkUser->getThisName();
+        $surname = $this->checkUser->getThisSurName();
+
+        $this->createDatabase();
+
+        $this->result = $this->getDB()->prepare("SELECT * FROM user_library WHERE name LIKE ? AND surname LIKE ?");
+
+        $this->result->execute(array($name . "%", $surname . "%"));
+
+        $row = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC), JSON_UNESCAPED_UNICODE);
+
+        print_r($row);
+
+        $this->closeConnection();
+    }
+
+    public function setUser()
+    {
+
+        $protected = $this->checkUser->userCheckAllParameters(true);
+
+        if (!$protected) {
+
+            return false;
+        }
+
+        $name = $this->checkUser->getThisName();
+        $surname = $this->checkUser->getThisSurName();
+        $passport_last = $this->checkUser->getThisPasswordLast();
+        $passport_first = $this->checkUser->getThisPasswordFirst();
+
+        $this->createDatabase();
+
+        $this->result = $this->getDB()->prepare(" SELECT `sp_check_user_add`(?, ?, ?, ?) AS `status`");
+
+        $this->result->execute(array($name, $surname, $passport_first, $passport_last));
+
+        $status = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC)[0]['status'], JSON_UNESCAPED_UNICODE);
+
+        $this->closeConnection();
+
+        if (+$status === 0) {
+
+            echo $this->messageResponse(400, 'user cannot be added. Passport details match with another user');
+
+            return false;
+        }
+
+        echo $this->messageResponse(201, 'user added successfully');
+    }
+
+    public function updateUser()
+    {
+
+        $protected = $this->checkUser->userUpdate();
+
+        if (!$protected) {
+
+            return false;
+        }
+
+        $id = $this->checkUser->getThisId();
+        $name = $this->checkUser->getThisName();
+        $surname = $this->checkUser->getThisSurName();
+        $passport_last = $this->checkUser->getThisPasswordLast();
+        $passport_first = $this->checkUser->getThisPasswordFirst();
+
+        $this->createDatabase();
+
+        $this->result = $this->getDB()->prepare(" SELECT `sp_update_user`(?, ?, ?, ?, ?) AS `status`");
+
+        $this->result->execute(array($id, $name, $surname, $passport_first, $passport_last));
+
+        $status = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC)[0]['status'], JSON_UNESCAPED_UNICODE);
+
+        $this->closeConnection();
+
+        if (+$status === 0) {
+
+            echo $this->messageResponse(400, 'error, user not found');
+
+            return false;
+        }
+
+        echo $this->messageResponse(201, 'user updated successfully');
+    }
+
+    public function deleteUser()
+    {
+
+        $protected = $this->checkUser->userUpdate();
+
+        if (!$protected) {
+
+            return false;
+        }
+
+        $id = $this->checkUser->getThisId();
+        $name = $this->checkUser->getThisName();
+        $surname = $this->checkUser->getThisSurName();
+        $passport_last = $this->checkUser->getThisPasswordLast();
+        $passport_first = $this->checkUser->getThisPasswordFirst();
+
+        $this->createDatabase();
+
+        $this->result = $this->getDB()->prepare(" SELECT `sp_delete_user`(?, ?, ?, ?, ?) AS `status`");
+
+        $this->result->execute(array($id, $name, $surname, $passport_first, $passport_last));
+
+        $status = json_encode($this->result->fetchAll(PDO::FETCH_ASSOC)[0]['status'], JSON_UNESCAPED_UNICODE);
+
+        $this->closeConnection();
+
+        if (+$status === 0) {
+
+            echo $this->messageResponse(400, 'error, user not found');
+
+            return false;
+        }
+
+        echo $this->messageResponse(201, 'user delete successfully');
     }
 }
