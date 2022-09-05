bc8d8611e2a771baab06b109f213e6e2bf30e577
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-09-05 14:12:59 +0300

    middelwear add

---
+++ src/middleware/middleware.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/../interface/interface.php';
+
+class Middleware implements IMiddleware
+{
+    private $fn;
+    private $nextValue = false;
+
+    public function setCallback($callback)
+    {
+
+        if (is_callable($callback)) {
+            $this->fn = $callback;
+        } else {
+            throw new Exception('Invalid function');
+        }
+
+        return $this;
+    }
+
+    public function setMiddleware($callback)
+    {
+
+        $this->nextValue = false;
+
+        if (is_callable($callback)) {
+
+            $callback(function () {
+
+                $this->nextValue = true;
+            });
+        } else {
+            throw new Exception('Invalid function');
+        }
+
+        if ($this->nextValue) {
+
+            return call_user_func($this->fn);
+        }
+
+        return $this;
+    }
+}
